313a8fe64c868d678553cf4ee4381ca0dfd11122
cRealNgineer <matyas.szabo2@stud.uni-corvinus.hu>
2021-11-25 19:08:39 +0100

    Excel feltöltése

--- ExcelExport/ExcelExport/Form1.cs
+++ ExcelExport/ExcelExport/Form1.cs
@@ -29,14 +29,89 @@ namespace ExcelExport
 
         private void CreateExcel() //hierarchikusan építjük fel
         {
-            xlApp = new Excel.Application();
-            xlWB = xlApp.Workbooks.Add();
-            xlSheet = xlWB.ActiveSheet;
+            try //más alkalmazásra hivatkozunk, ami sok hibához vezethet
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add();
+                xlSheet = xlWB.ActiveSheet;
 
-            //CreateTable();
+                //CreateTable();
 
-            xlApp.Visible = true;
-            xlApp.UserControl = true;
+                xlApp.Visible = true; //enélkül nem jelenik meg az excel!!
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}: ", ex.Message, ex.Source);
+                MessageBox.Show(errMsg);
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+
+        }
+
+        private void CreateTable()
+        {
+            string[] headers = new string[] 
+            {
+                "Kód",
+                "Eladó",
+                "Oldal",
+                "Kerület",
+                "Lift",
+                "Szobák száma",
+                "Alapterület (m2)",
+                "Ár (mFt)",
+                "Négyzetméter ár (Ft/m2)"
+            };
+
+            for (int i = 1; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i] = headers[i - 1];
+            }
+
+            object[,] values = new object[Flats.Count, headers.Length];
+
+            int counter = 0;
+
+            foreach (Flat item in Flats) //object feltöltése
+            {
+                values[counter, 0] = item.Code;
+                values[counter, 1] = item.Vendor;
+                values[counter, 2] = item.Side;
+                values[counter, 3] = item.District;
+                values[counter, 4] = item.Elevator;
+                values[counter, 5] = item.NumberOfRooms;
+                values[counter, 6] = item.FloorArea;
+                values[counter, 7] = item.Price;
+                values[counter, 8] = "";
+                counter++;
+            }
+
+            xlSheet.get_Range(
+             GetCell(2, 1),
+             GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+
+        }
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
         }
 
         private void LoadData()
