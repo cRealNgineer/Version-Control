2f500c169c3377650648168e21ecfccadbf0889c
cRealNgineer <matyas.szabo2@stud.uni-corvinus.hu>
2021-11-26 16:06:05 +0100

    Datagridview feltöltése

--- ValueAtRisk/ValueAtRisk/Form1.cs
+++ ValueAtRisk/ValueAtRisk/Form1.cs
@@ -7,14 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ValueAtRisk.Entities;
 
 namespace ValueAtRisk
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+
         public Form1()
         {
             InitializeComponent();
+            ticks = context.Tick.ToList();
+            dataGridView1.DataSource = ticks;
+
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            PortfolioItem p = new PortfolioItem();
+            /*p.Index = OTP";
+            p.Volume = 10;
+            Portfolio.Add(p);*/ //ugyanaz mint lent
+
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
