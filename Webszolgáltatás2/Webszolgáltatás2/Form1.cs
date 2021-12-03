46cb8f0f8d7463bfe1388c8e0e131be9bc26f2be
cRealNgineer <matyas.szabo2@stud.uni-corvinus.hu>
2021-12-03 17:58:23 +0100

    Bindinglist feltöltése

--- Webszolgáltatás2/Webszolgáltatás2/Form1.cs
+++ Webszolgáltatás2/Webszolgáltatás2/Form1.cs
@@ -7,14 +7,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using Webszolgáltatás2.Entities;
+using Webszolgáltatás2.MNBServiceReference;
 
 namespace Webszolgáltatás2
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> _rates = new BindingList<RateData>();
+
         public Form1()
         {
             InitializeComponent();
+            loadXml(getRates());
+            dataGridView1.DataSource = _rates;
         }
+
+        private void loadXml(string xmlstring)
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(xmlstring);
+
+            foreach (XmlElement item in xml.DocumentElement)
+            {
+                RateData r = new RateData();
+                r.Date = DateTime.Parse(item.GetAttribute("date"));
+                var childElement = (XmlElement)item.ChildNodes[0];
+                r.Currency = childElement.GetAttribute("curr");
+                decimal unit = decimal.Parse(childElement.GetAttribute("unit"));
+                r.Value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                {
+                    r.Value = r.Value / unit;
+                }
+                _rates.Add(r);
+            }
+        }
+
+        private string getRates()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            GetExchangeRatesRequestBody req = new GetExchangeRatesRequestBody();
+            req.currencyNames = "EUR";
+            req.startDate = "2020-01-01";
+            req.endDate = "2020-06-30";
+            var response = mnbService.GetExchangeRates(req);
+            return response.GetExchangeRatesResult;
+        }
+
+
     }
 }
