453825e036c884369b061796f2c3f4e109d87f66
cRealNgineer <matyas.szabo2@stud.uni-corvinus.hu>
2021-12-03 16:00:46 +0100

    Ball create

---
+++ Programtervezési minták/Programtervezési minták/Entites/Balls.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programtervezési_minták.Entites
+{
+    public class Balls : Label
+    {
+        public Balls()
+            {
+            AutoSize = false;
+            Width = Height = 50;
+            Paint += Balls_Paint;
+            }
+
+        private void Balls_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        private void DrawImage(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Red), 0, 0, Width, Height);
+        }
+
+        public void MoveBall()
+        {
+            Left++;
+        }
+    }
+}
---
+++ Programtervezési minták/Programtervezési minták/Form1.cs
@@ -0,0 +1,47 @@
+﻿using Programtervezési_minták.Entites;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programtervezési_minták
+{
+    public partial class Form1 : Form
+    {
+        List<Balls> _balls = new List<Balls>();
+        private BallFactory ballFactory;
+
+        public BallFactory Factory
+        {
+            get { return ballFactory; }
+            set { ballFactory = value; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            Factory = new BallFactory();
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            Balls b = Factory.CreateNew();
+            _balls.Add(b);
+            b.Left = -b.Width;
+            mainPanel.Controls.Add(b);
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            foreach (Balls item in _balls)
+            {
+                item.MoveBall();
+            }
+        }
+    }
+}
