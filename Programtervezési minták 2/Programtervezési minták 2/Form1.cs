e5011d9d617b97bddee4ba202a26a28ef9c940de
cRealNgineer <matyas.szabo2@stud.uni-corvinus.hu>
2021-12-10 14:02:34 +0100

    Ball on Form

--- Programtervezési minták 2/Programtervezési minták 2/Form1.cs
+++ Programtervezési minták 2/Programtervezési minták 2/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Programtervezési_minták_2.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,35 @@ namespace Programtervezési_minták_2
 {
     public partial class Form1 : Form
     {
+        List<Ball> _balls = new List<Ball>();
+        private BallFactory myV_factoryar;
+
+        public BallFactory Factory
+        {
+            get { return myV_factoryar; }
+            set { myV_factoryar = value; }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            Factory = new BallFactory();
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            Ball b = Factory.CreateNew();
+            _balls.Add(b);
+            b.Left = -b.Width;
+            mainPanel.Controls.Add(b);
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            foreach (Ball item in _balls)
+            {
+                item.MoveBall();
+            }
         }
     }
 }
