0c3f8207a184a79f13245e87a30329887ce1850f
cRealNgineer <matyas.szabo2@stud.uni-corvinus.hu>
2021-12-17 16:58:40 +0100

    Final

--- WorldsHardestGame/WorldsHardestGame/Form1.Designer.cs
+++ WorldsHardestGame/WorldsHardestGame/Form1.Designer.cs
@@ -29,13 +29,48 @@ namespace WorldsHardestGame
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblGeneration = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblGeneration
+            // 
+            this.lblGeneration.AutoSize = true;
+            this.lblGeneration.Location = new System.Drawing.Point(364, 376);
+            this.lblGeneration.Name = "lblGeneration";
+            this.lblGeneration.Size = new System.Drawing.Size(66, 13);
+            this.lblGeneration.TabIndex = 0;
+            this.lblGeneration.Text = "1. generáció";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(367, 403);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(63, 23);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Visible = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.lblGeneration);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblGeneration;
+        private System.Windows.Forms.Button startButton;
     }
 }
 
